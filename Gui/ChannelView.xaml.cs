a8e251bd328d28d91871d6eec0db457566786cbb
FunkyPancake <43291121+FunkyPancake@users.noreply.github.com>
2022-03-21 04:15:30 +0100

    Merge pull request #2
    * add periodic timer
    
    * cleanup gui

--- Gui/ChannelView.xaml.cs
+++ Gui/ChannelView.xaml.cs
@@ -1,4 +1,7 @@
+using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using Controller;
 
 namespace RemotePowerSupplyGui;
@@ -12,5 +15,35 @@ public partial class ChannelView : UserControl
         Channel = channel;
         InitializeComponent();
         ChannelLabel.Content = $"Channel {Channel.Id}";
+    }
+
+    private void OutputButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (Channel.OutputEnable)
+        {
+            Channel.OutputEnable = true;
+            OutputButton.Content = "Disable";
+        }
+        else
+        {
+            Channel.OutputEnable = false;
+            OutputButton.Content = "Enable";
+        }
+    }
+
+    public void RefreshView()
+    {
+        VoltageTextBox.Text = Channel.Voltage.ToString("F3",CultureInfo.InvariantCulture);
+        CurrentTextBox.Text = Channel.Current.ToString("F3",CultureInfo.InvariantCulture);
+        if (Channel.OutputEnable)
+        {
+            OutputStatusTextBox.Text = "On";
+            OutputStatusTextBox.Background = new SolidColorBrush(Colors.Chartreuse);
+        }
+        else
+        {
+            OutputStatusTextBox.Text = "Off";
+            OutputStatusTextBox.Background = new SolidColorBrush(Colors.Crimson);
+        }
     }
 }
